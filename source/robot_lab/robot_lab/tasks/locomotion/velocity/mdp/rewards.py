fixed bug in velocity tracking reward calculation

source/robot_lab/robot_lab/tasks/locomotion/velocity/mdp/rewards.py
@@ -156,7 +156,7 @@ def skate_rot_penalty(
 ) -> torch.Tensor:
     """Penalize relative (between robot and skateboard) frame orinetation error in vicinity of skateboard"""
     # extract the used quantities (to enable type-hinting)
-    vicinity_radius = 0.7
+    vicinity_radius = 0.5
     skate_rot_rel = env.scene["skate_transform"].data.target_quat_source.squeeze(1)
     skate_angle_rel = 2 * torch.acos(torch.clamp(torch.abs(skate_rot_rel[:, 0]), max=1.0))
     distance = torch.linalg.norm(env.scene["skate_transform"].data.target_pos_source.squeeze(1), dim=1)
@@ -177,6 +177,7 @@ def skate_track_lin_vel_xy_exp(
     skate_asset: RigidObject = env.scene[skate_asset_cfg.name]
 
     target_vel = skate_asset.data.root_pos_w[:, :2] - robot_asset.data.root_pos_w[:, :2]
+    # target_vel = robot_asset.data.root_pos_w[:, :2] - skate_asset.data.root_pos_w[:, :2]
     norm_v = torch.norm(target_vel, dim=1, keepdim=True)
     norm_v = norm_v.repeat(1, 2)
     vector_norm = 0.5
@@ -185,7 +186,7 @@ def skate_track_lin_vel_xy_exp(
 
     # compute the error
     lin_vel_error = torch.sum(
-        torch.square(target_vel - robot_asset.data.root_lin_vel_b[:, :2]),
+        torch.square(target_vel - robot_asset.data.root_lin_vel_w[:, :2]),
         dim=1,
     )
     reward = torch.exp(-lin_vel_error / std**2)